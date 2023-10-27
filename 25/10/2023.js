6dc089191c41d61e1612dcf6cef51bad31a686ed
Deepak Sharma <147157640+deepak132802@users.noreply.github.com>
2023-10-27 18:43:16 +0530

    Create 2023.js

---
+++ 25/10/2023.js
@@ -0,0 +1,28 @@
+// value swap
+
+let a="rakesh"
+let b="shyam"
+let c=""
+
+
+// console.log(a)
+// console.log(b)
+
+c-a;
+a-b;
+b-c;
+
+console.log(a)
+console.log(b)
+
+
+
+// sum of numbers
+
+let d = 0;
+
+for (let i = 1; i <= 20; i++) {
+  d += i;
+}
+
+console.log("The sum of numbers from 1 to 20 is: " + d);
